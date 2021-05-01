e3f7c8451dd8f31c5c8012796b63f289ec5d8e3c
Idrees <57256007+IdreesSamadi@users.noreply.github.com>
2021-05-01 08:04:17 +0200

    fixing bug with user schema

--- backend/src/data/users.ts
+++ backend/src/data/users.ts
@@ -3,21 +3,21 @@ import bcrypt from 'bcryptjs'
 const users = [
   {
     name: 'Admin User',
-    email: 'admin@example.com',
+    email: 'admin@test.com',
     password: bcrypt.hashSync('password', 10),
     isAdmin: true
   },
   {
-    name: 'John Doe',
-    email: 'John@example.com',
+    name: 'Tom Car',
+    email: 'tom@test.com',
     password: bcrypt.hashSync('password', 10),
-    isAdmin: true
+    isAdmin: false
   },
   {
-    name: 'Jane Doe',
-    email: 'Jane@example.com',
+    name: 'Jerry Mouse',
+    email: 'jerry@test.com',
     password: bcrypt.hashSync('password', 10),
-    isAdmin: true
+    isAdmin: false
   }
 ]
 
