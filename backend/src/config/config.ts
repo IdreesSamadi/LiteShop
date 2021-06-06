1af41762df757c2d10f366c966592726966298b8
Idrees <57256007+IdreesSamadi@users.noreply.github.com>
2021-06-06 21:00:57 +0200

    updating the package and the config file for the production

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -18,7 +18,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost'
-const SERVER_PORT = process.env.SERVER_PORT || 1337
+const SERVER_PORT = process.env.PORT || 1337
 const NODE_ENV = process.env.NODE_ENV || 'development'
 const MONGO_URI =
   process.env.MONGO_URI ||
